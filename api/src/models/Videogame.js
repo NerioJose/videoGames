e65b7ec244c4481e1c72b69da47ba980e5e3ea71
NerioJose <neriojose531@gmail.com>
2024-07-31 17:46:12 -0500

    Cambio del script start en el package json para el despliegue de la aplicacion

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   sequelize.define(
-    'Videogame',  // Cambiar a 'Videogame' para que coincida
+    'Videogame',  
     {
       id: {
         type: DataTypes.UUID,
