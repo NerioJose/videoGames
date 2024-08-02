c068dc3e6d32fff5f261061da3892decd7729211
NerioJose <neriojose531@gmail.com>
2024-08-02 18:12:03 -0500

    Correccion de rutas

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import './index.css';
+import axios from 'axios';
 
-
-;
+axios.defaults.baseURL='https://server-videogames-aqbh.onrender.com';
 
 ReactDOM.render(
   <Provider store={store}>
