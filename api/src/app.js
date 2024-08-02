82136ddbffe5200e3868fc61da012eaf702208f6
NerioJose <neriojose531@gmail.com>
2024-08-02 17:51:18 -0500

    Correccion de bug de rutas

--- api/src/app.js
+++ api/src/app.js
@@ -43,14 +43,3 @@ server.use((err, req, res, next) => {
 });
 
 module.exports = server;
-
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001; // Cambiado de { PORT } a PORT
-
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`listening at Port ${PORT}`); // eslint-disable-line no-console
-  });
-});
