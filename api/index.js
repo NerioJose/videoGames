c58483e18bf5696c233d8ff36b6540c9093e5137
Nerio <neriojose531@gmail.com>
2026-01-22 15:03:58 -0500

    preparando api para vercel

--- api/index.js
+++ api/index.js
@@ -1,29 +1,18 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env.PORT  || 3001;
+const PORT = process.env.PORT || 3001;
 
-// Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`listening at Port ${PORT}`); // eslint-disable-line no-console
+// Para Vercel (Serverless)
+module.exports = async (req, res) => {
+  await conn.sync({ alter: true });
+  server(req, res);
+};
+
+// Para Local
+if (require.main === module) {
+  conn.sync({ alter: true }).then(() => {
+    server.listen(PORT, () => {
+      console.log(`listening at Port ${PORT}`); // eslint-disable-line no-console
+    });
   });
-});
+}
