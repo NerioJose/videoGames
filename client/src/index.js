0f46b385d3b6ab02b91476a9f5ead3d3b9ea3db4
Nerio <neriojose531@gmail.com>
2026-01-22 15:39:40 -0500

    fix client build remove dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import App from './App';
 import './index.css';
 
 import axios from 'axios';
-import dotenv from "dotenv";
-dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
